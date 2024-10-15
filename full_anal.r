2ba2b32068a71126afe2261052bdd658b10cd36a
inga420 <60630082+inga420@users.noreply.github.com>
2024-10-15 12:01:22 +0200

    file for full analysis

---
+++ full_anal.r
@@ -0,0 +1,24 @@
+library(lavaan)
+library(dagitty)
+library(tidyverse)
+library(ggdag)
+
+data <- read_csv("data.csv")
+# not specifying column data types here, because:
+# 1) we don't need to distinguish between doubles and ints except for optimization (which we could eventually do, sure)
+# 2) I'll be changing to factors in the next chunk anyway
+
+# changing categorical variables to R factors
+data <- data |>
+  mutate(
+    sex = factor(sex),
+    cp  = factor(cp) ,
+    fbs = factor(fbs),
+    restecg = factor(restecg),
+    exang = factor(exang),
+    slope = factor(slope),
+    thal  = factor(thal)
+  )
+
+# make descriptive plots
+
