4ac92ef4c6722759653ec3fe5229c20c3234dedd
Inga Schöyen <93376973+ingaschoeyen@users.noreply.github.com>
2024-10-18 22:01:18 +0200

    Update data_import_viz.R

--- data_import_viz.R
+++ data_import_viz.R
@@ -4,6 +4,8 @@ library(ggdag)
 library(ggplot2)
 library(tidyverse)
 
+
+
 # Setup functions -----------------------------------------------------------------------------
 load_dag <- function(filepath) {
   dagtxt <- read_file(filepath)
@@ -11,12 +13,12 @@ load_dag <- function(filepath) {
   return(dag)
 }
 
+
 local_tests_utility <- function(dag, M, data) {
   # local chi-square tests
-  t <- localTests(x=dag, sample.cov=M, sample.nobs=nrow(data))
-  plotLocalTestResults(t)
-  
-  return(t)
+  t <- localTests(x = dag, sample.cov = M, sample.nobs = nrow(data))
+  ttest_fig <- plotLocalTestResults(t)
+  return(list(test = t, fig = ttest_fig))
 }
 
 # Load Data -----------------------------------------------------------------------------------
@@ -28,7 +30,7 @@ data <- read.csv("data.csv")
 # drop NA
 data <- data[complete.cases(data),]
 # drop index
-data <- subset(data, select=-c(X))
+data <- data[-1, ]
 
 # changing categorical variables to R factors
 # all ordered for now, subject to change
@@ -44,6 +46,8 @@ data <- data |>
     num     = factor(num, ordered = TRUE)
   )
 
+print("test 4.1")
+
 # scale appropriate variables for comparisons
 data <- data |>
   mutate(
@@ -55,17 +59,25 @@ data <- data |>
     ca       = scale(ca)
   )
 
+
+print(summary(data))
+# change var names to match DAG
+colnames(data) <- c("ID", "AGE", "SEX", "CP", "BPr", "Cho", "FBS", "ECr", "HRm", "ANe", "STd", "STs", "Tha", "CA","HD")
+
 # Getting lavaan correlation matrix
 M <- lavCor(data)
-varTable(data)
+print(varTable(data))
 
 # Run tests -----------------------------------------------------------------------------------
 dagpaths = c("stian_dag.txt", "DAGcode_old_var_names.txt")
 daglist <- c()
+i <- 1
 
 for (dagpath in dagpaths) {
   dag <- load_dag(dagpath)
-  print(ggdag(dag))
+  dag_fig <- ggdag(dag)
+  ggsave(filename = paste("dag_plot", i, ".png", sep = "_"), plot = dag_fig, path = "./plots/", width = 6, height = 4, units = "in", dpi = 300)
   daglist <- c(daglist, dag)
-  local_tests_utility(dag, M, data)
+  ttest_results <- local_tests_utility(dag, M, data)
+  i <- i + 1
 }
