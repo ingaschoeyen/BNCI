de1c1853e52618a3eee680b831707393d6ae1f57
Inga Schöyen <93376973+ingaschoeyen@users.noreply.github.com>
2024-10-18 22:11:52 +0200

    Update data.py

--- data.py
+++ data.py
@@ -1,6 +1,6 @@
 from ucimlrepo import fetch_ucirepo
 import pandas as pd
-
+import numpy as np
 """
 fetches dataset from uciml repo and saves to csv, so that we can use it in R
 """
@@ -13,12 +13,23 @@ X = heart_disease.data.features
 y = heart_disease.data.targets 
   
 # metadata 
-print(heart_disease.metadata) 
+# print(heart_disease.metadata) 
   
 # variable information 
 print(heart_disease.variables)
 
 z = X.join(y)
-z.to_csv("data.csv")
+# z.to_csv("data.csv")
+
+
+
+# print(z.head())
+
+for var in z.columns:
+    print(var)
+    print(f"No NAN:{z[var].isnull().sum()}")
+    print(f"Mean of {var}: {np.mean(z[var])}")
+    print(f"Min of {var}: {np.min(z[var])}")
+    print(f"Max of {var}: {np.max(z[var])}")
 
 print("Done")
