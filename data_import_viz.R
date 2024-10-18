2c5bc469761225ce59882b0b5409f77e4d79f64a
Stian S. Grønlund <stian.groenlund@ru.nl>
2024-10-18 14:31:49 +0200

    prog

--- data_import_viz.R
+++ data_import_viz.R
@@ -1,14 +1,20 @@
 library(lavaan)
 library(dagitty)
-library(tidyverse)
 library(ggdag)
+library(tidyverse)
 
-# dag from file
-filepath = "DAGcode_old_var_names.txt"
-dagtxt <- read_file(filepath)
-dag <- dagitty(dagtxt)
-ggdag(dag)
+# Setup functions -----------------------------------------------------------------------------
+load_dag <- function(filepath) {
+  dagtxt <- read_file(filepath)
+  dag <- dagitty(dagtxt)
+  ggdag(dag)
+  
+  return(dag)
+}
 
+
+
+# Load Data -----------------------------------------------------------------------------------
 data <- read.csv("data.csv")
 # not specifying column data types here, because:
 # 1) we don't need to distinguish between doubles and ints except for optimization (which we could eventually do, sure)
@@ -18,26 +24,38 @@ data <- read.csv("data.csv")
 data <- data[complete.cases(data),]
 # drop index
 data <- subset(data, select=-c(X))
-# scale everything for comparisons
-data <- apply(data, 2, scale)
-
 
 # changing categorical variables to R factors
+# all ordered for now, subject to change
 data <- data |>
   mutate(
-    sex = factor(sex, ordered=TRUE),
-    cp  = factor(cp, ordered=TRUE) ,
-    fbs = factor(fbs, ordered=TRUE),
+    sex     = factor(sex, ordered=TRUE),
+    cp      = factor(cp, ordered=TRUE) ,
+    fbs     = factor(fbs, ordered=TRUE),
     restecg = factor(restecg, ordered=TRUE),
-    exang = factor(exang, ordered=TRUE),
-    slope = factor(slope, ordered=TRUE),
-    thal  = factor(thal, ordered=TRUE)
+    exang   = factor(exang, ordered=TRUE),
+    slope   = factor(slope, ordered=TRUE),
+    thal    = factor(thal, ordered=TRUE),
+    num     = factor(num, ordered = TRUE)
   )
 
-M <- lavCor(data)
+# scale appropriate variables for comparisons
+data <- data |>
+  mutate(
+    age      = scale(age),
+    trestbps = scale(trestbps),
+    chol     = scale(chol),
+    thalach  = scale(thalach),
+    oldpeak  = scale(oldpeak),
+    ca       = scale(ca)
+  )
 
+# Getting lavaan correlation matrix
+M <- lavCor(data)
 varTable(data)
 
 # local chi-square tests
-t <- localTests(x=dag, sample.cov=M, sample.nobs=nrow(data))
+t <- localTests(x=dag0, sample.cov=M, sample.nobs=nrow(d))
 plotLocalTestResults(t)
+
+
