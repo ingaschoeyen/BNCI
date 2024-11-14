0dd4871b0846018635340df3726071cb06f4054c
inga420 <60630082+inga420@users.noreply.github.com>
2024-11-14 21:36:49 +0100

    changed up figure

--- data_pre.r
+++ data_pre.r
@@ -1,7 +1,7 @@
 library(tidyr)
 library(dplyr)
 library(ggplot2)
-library(gridExtra)
+library(patchwork)
 
 data_path <- 'data.csv'
 
@@ -46,37 +46,30 @@ data <- read.csv(data_path)
 df_long <- data %>%
   pivot_longer(cols = -subject_id, names_to = "variable", values_to = "value")
 
-# Create the plot with facets
-fig_scatter <- ggplot(df_long, aes(x = variable, y = value)) +
-  geom_jitter(width = 0.2, alpha = 0.6) +  # Adds some horizontal jitter to separate points
-  labs(x = "Variable", y = "Value", title = "Scatter Plot of Variable Value Distributions") +
-  theme_minimal() +
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
-
-print(fig_scatter)
-
 # Define high-scale and low-scale variable sets
 high_scale_vars <- c("trestbps", "chol", "thalach")  # Variables with larger scales
-non_low <- c("age", "trestbps", "chol", "thalach", "sex")
+non_low <- c("age", "trestbps", "chol", "thalach", "sex") # includes age and sex, since we dont really care about distribution
 low_scale_vars <- setdiff(unique(df_long$variable), non_low)  # The rest
 
 # Plot high-scale variables (left side)
 plot_high <- ggplot(df_long %>% filter(variable %in% high_scale_vars), aes(x = variable, y = value)) +
   geom_jitter(width = 0.2, alpha = 0.6) +
-  scale_y_continuous(name = "High Scale Values") +  # Left y-axis label
-  labs(x = NULL, title = "High-Scale Variables") +
+  scale_y_continuous(name = "") +  # Left y-axis label
+  labs(x = NULL, title = "") +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
 
 # Plot low-scale variables (right side)
 plot_low <- ggplot(df_long %>% filter(variable %in% low_scale_vars), aes(x = variable, y = value)) +
-  geom_jitter(width = 0.2, alpha = 0.6) +
-  scale_y_continuous(name = "Low Scale Values", position = "right") +  # Right y-axis label
-  labs(x = NULL, title = "Low-Scale Variables") +
+  geom_jitter(width = 0.3, alpha = 0.2) +
+  scale_y_continuous(name = "", position = "right") +  # Right y-axis label
+  labs(x = NULL, title = "") +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
 
 # Combine the plots side by side
-plot_both <- gridExtra::grid.arrange(plot_high, plot_low, ncol = 2)
+# plot_both <- gridExtra::grid.arrange(plot_high, plot_low, ncol = 2)
 
-ggsave(filename = "data_scatter.png", plot = plot_both, path = "./plots/", width = 6, height = 4, units = "in", dpi = 300)
+plot_both <- plot_high + plot_low + plot_layout(widths = c(1, 2))
+
+ggsave(filename = "data_scatter.png", plot = plot_both, path = "./plots/", width = 8, height = 4, units = "in", dpi = 300)
